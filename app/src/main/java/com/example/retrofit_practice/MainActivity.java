2cd09cd42d3c0ed80bdd59e85ffbd41d79236aa1
savva <zulvit.sk@gmail.com>
2023-10-21 11:47:25 +0300

    - bugfix

--- app/src/main/java/com/example/retrofit_practice/MainActivity.java
+++ app/src/main/java/com/example/retrofit_practice/MainActivity.java
@@ -1,10 +1,10 @@
 package com.example.retrofit_practice;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
